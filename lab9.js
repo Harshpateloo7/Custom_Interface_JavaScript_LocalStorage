efc03314fc984028485ac7e6100be4806118646f
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2022-11-21 23:22:11 -0500

    Custom interface using JavaScript

---
+++ lab9.js
@@ -0,0 +1,46 @@
+﻿//LAB 9-DATA STORAGE: INDEX PAGE
+window.onload = goLive;
+
+function goLive(){
+    var msgOut = document.getElementById("newMsgBox");
+    
+    // Not the part of Program
+    // msgOut.innerHTML = "Welcome: " + name_f;
+    // document.cookie = "name_f; max-age=600";
+    // console.log(document.cookie);
+    // document.cookie ="color_f; max-age=600";
+    // console.log(document.cookie); 
+
+
+    var formHandle = document.forms.infoForm;
+    formHandle.onsubmit = storeValue;
+    //STore value funcation for the storing value of Name and color
+    function storeValue(){
+        var name_f = formHandle.f_name.value;
+        var color_f = formHandle.f_color.value;
+        // Set the value on local storage
+        localStorage.setItem("name_f", name_f);
+        localStorage.setItem("color_f",color_f);
+    }
+    // Get the value from user and store in local storage
+    var getName = localStorage.getItem("name_f");
+    var getColor = localStorage.getItem("color_f");
+    // checking if color and name if not equal to null 
+    if(getColor !== null){
+        document.body.style.backgroundColor = getColor;
+    }
+    if (getName !== null){
+        msgOut.innerHTML = " Welcome " + getName;
+    }
+    // Detele storage value function
+    var del = document.getElementById("btnDel");
+    del.onclick = removeI;
+    
+    function removeI(){
+    window.location.reload(); 
+    localStorage.removeItem("name_f");
+    localStorage.removeItem("color_f");
+      
+    }
+
+}
---
+++ products.js
@@ -0,0 +1,71 @@
+﻿//LAB 9-DATA STORAGE: PRODUCTS PAGE
+window.onload = goLive;
+
+function goLive(){
+    var msgOut = document.getElementById("newMsgBox");
+    // msgOut.innerHTML = "Welcome: " + name_f;
+
+    // document.cookie = "f_name; max-age=600";
+    // console.log(document.cookie);
+
+
+    // var formHandle = document.forms.infoForm;
+    // formHandle.onsubmit = storeValue;
+
+    // function storeValue(){
+    //     var name_f = formHandle.f_name.value;
+    //     var color_f = formHandle.f_color.value;
+
+    //     localStorage.setItem("name_f", name_f);
+    //     localStorage.setItem("color_f",color_f);
+    // }
+    var getName = localStorage.getItem("name_f");
+    var getColor = localStorage.getItem("color_f");
+
+    if(getColor !== null){
+        document.body.style.backgroundColor = getColor;
+    }
+    if (getName !== null){
+        msgOut.innerHTML = " Welcome " + getName;
+    }
+}
+
+
+
+// window.onload = function(){
+   
+
+
+
+
+// //     //
+// //     document.cookie ="language=enca;max-age=10";
+// //     console.log(document.cookie);
+// //     //
+// //    var nameCookie = document.cookie;
+// //    outMsg.innerHTML = "Full cookie: " + nameCookie;
+
+// //    var startIndex = nameCookie.indexOf("e=");
+// //    outMsg.innerHTML+= "<br/>Foun at: " + startIndex;
+
+// //    var f_name = nameCookie.substring(startIndex + 2);
+// //    outMsg.innerHTML = "First Name stored in cookie: " + f_name;
+
+// //    document.cookie = "favTeam=Raptors; max-age=3600;";
+// //    outMsg.innerHTML = "Mulitple Cookie: " +document.cookie;
+// //    console.log(document.cookie);
+
+// //    var cookieArray = document.cookie.split(";")
+// //    console.log(cookieArray);
+
+// //    var teamKeyValue = cookieArray[1];
+// //    outMsg.innerHTML+="<br/>cookieArray[1]: " +teamKeyValue;
+
+// //    var team = teamKeyValue.split("=")[1];
+// //    console.log("fav Team: " + team);
+
+// //    var shortCut = cookieArray[1].split("=")[1];
+// //    console.log("Shortcut to Team: " +shortCut);
+
+// }
+
